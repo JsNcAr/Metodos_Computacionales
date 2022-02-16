cf884bad664999c46ad44d0b7ee16cd8725a26e7
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-02-16 13:48:29 -0500

    Se agrega variable binaria

--- Semana_4/Archivo.cpp
+++ Semana_4/Archivo.cpp
@@ -18,18 +18,22 @@ int main()
 
     int s;
 
-    //cin le pide input al usuario un imput y lo guarda en la variable indicada
-    cout<<"ingrese valor ";cin>> s;cout<<"el valor ingresado fue :"<<s<<endl;
+    //cin le pide  al usuario un input y lo guarda en la variable indicada
+    //cout<<"ingrese valor ";cin>> s;cout<<"el valor ingresado fue :"<<s<<endl;
 
 
     //const lockea una variable a un valor determinado
     const char CONSTANTE[] = "Constante";
 
-    //SE cambia el valor de la variable a
+    //Se cambia el valor de la variable a
     a = 10;
 
     cout<<"Nueva a: "<< a<<endl;
 
+    //Se crea una variable binaria
+    int v = 0b11;
+
+    cout<<"Numero binario: "<<v<<endl;
 
     return 0;
 }
