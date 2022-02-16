ec57c0ff3ecb56cf4e4ab35358bdd4a4e8e77a7d
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-02-16 13:42:56 -0500

    Se ven los primeros metodos y funciones de c++

--- Semana_4/Archivo.cpp
+++ Semana_4/Archivo.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-#include <ostream>
+#include <string>
+#include <math.h>  
 
+using namespace std;//Para remover la necesidad de llamar a std cada vez que se use la libreria estandar
 int main()
 {
-    std::cout<<"Hola Mundo"<<std::endl;
+    //Enviar mensaje a la consola
+    cout<<"Hola Mundo"<<endl;
     
+    int a = 2;
+
+    float b = 5.;
+
+    double c = 5*pow(10,-10); //double tiene mas precision
+
+    cout<<"a = "<< a << " b = " << b << " c = " << c <<endl;
+
+    int s;
+
+    //cin le pide input al usuario un imput y lo guarda en la variable indicada
+    cout<<"ingrese valor ";cin>> s;cout<<"el valor ingresado fue :"<<s<<endl;
+
+
+    //const lockea una variable a un valor determinado
+    const char CONSTANTE[] = "Constante";
+
+    //SE cambia el valor de la variable a
+    a = 10;
+
+    cout<<"Nueva a: "<< a<<endl;
+
+
     return 0;
 }
