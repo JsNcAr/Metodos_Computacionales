e74dea6baa1977d779193d3c8174e3636cd3f0b5
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-02-23 12:38:24 -0500

    Create new folder

---
+++ Semana_05/archivo.cpp
@@ -0,0 +1,15 @@
+ //g++ -std=c++14 -Wall -Wextra -pedantic Plantilla.cpp -o exe && ./exe
+
+#include <iostream>
+#include <string>
+#include <math.h>  
+
+using namespace std;//Para remover la necesidad de llamar a std cada vez que se use la libreria estandar
+
+
+int main(){
+
+    cin>>a>>endl;
+    cout<<a<<endl;
+    return 0;
+}
