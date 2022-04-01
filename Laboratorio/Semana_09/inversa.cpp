350478c49953fabe5f58d4d5e1f82ba980a00919
Jason Nicolas Arias Gomez <UANDES\jn.arias@fisicaey110b19l.ad.uniandes.edu.co>
2022-04-01 12:35:33 -0500

    There was an attempt

--- Laboratorio/Semana_09/inversa.cpp
+++ Laboratorio/Semana_09/inversa.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std; //Se importa std
 
-const int N = 4;
+const int N = 3;
 int i,j,k;
 float r;
 
@@ -19,7 +19,7 @@ void print__(float A[N][N]){
 	}
 }
 
-void unificacion(float A[N][N]){
+void unificacion(float A[N][N], float B[N][N]){
 	for(i = 0; i < N; i++) {
 		
 		r = A[i][i];
@@ -27,47 +27,59 @@ void unificacion(float A[N][N]){
 		for(j = i; j < N; j++){
 			
 			A[i][j] /= r;
+			B[i][j] /= r;
 		}
 	}
 }
 
-int main()
-{
-	
-	float A[N][N] = {{2,0,1,0},{2,1,1,0},{1,0,-3,0},{3,0,2,1}};
-	
-	
-	
-	/* Se transforman los pivotes en 1 */
-	unificacion(A);
-	
-	/* Se eliminan los elementos sobrantes de las columnas intermedias */
-
+void intermedias(float A[N][N], float B[N][N]){
 	for(i = 0; i < N; i++){
 		for(j = i+1; j < N; j++){
 			r = A[j][i];
 			if(r != 0){
 				for(k = 0; k < N; k++){
 					A[j][k] -=  r*A[i][k];
+					B[j][k] -=  r*A[i][k];
 				}
 			}
 		}
-	}
+	}	
+}
 
-	
-	/* Se vuelven a unificar los pivotes */
-	unificacion(A);
-	
-	/* Se opera la ultima fila */
+void ultima(float A[N][N], float B[N][N]){
 	for(i = 0; i < N-1; i++){
 		r = A[N-1][i];
 		if(r != 0){
 			for(j = 0; j < N; j++){
 				A[N-1][i] -= r*A[i][j];
+				B[N-1][i] -= r*A[i][j];
 			}
 		}
-	}	
-	print__(A);
+	}
+}
+int main()
+{
+	
+	float A[N][N] = {{2,0,1},{0,-2,0},{0,0,2}};
+	float B[N][N] = {{1,0,0},{0,1,0},{0,0,1}};
+	
+	
+	
+	/* Se transforman los pivotes en 1 */
+	unificacion(A, B);
+	
+	/* Se eliminan los elementos sobrantes de las columnas intermedias */
+
+	intermedias(A, B);
+
+	
+	/* Se vuelven a unificar los pivotes */
+	unificacion(A, B);
+	
+	/* Se opera la ultima fila */
+	ultima(A, B);
+	
+	print__(B);
 }
 
 
